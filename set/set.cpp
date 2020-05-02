93d785e8ab7056ccf2b63d919f45cb548f801152
raoxuntian <raoxuntian@gmail.com>
2020-05-02 22:45:51 +0800

    add something

--- set/set.cpp
+++ set/set.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
 #include <set>
 #include <vector>
+#include <unordered_set>
 using namespace std;
+
+struct myCmp {
+	bool operator() (const int &a,const int &b) {
+		return !!(a>b);
+	}
+};
 
 int main(int argc, char const *argv[])
 {
-	int ia[10] = {1, 3, 5, 8, 9, 1, 2, 3, 9, 6};
-	std::vector<int> v(ia, ia+10);
+	int ia[12] = {10, -5 ,1, 3, 5, 8, 9, 1, 2, 3, 9, 6};
+	std::vector<int> v(ia, ia+12);
 	set<int> iset(v.begin(), v.end());
+	set<int, myCmp> newiset(v.begin(), v.end());
+	unordered_set<int> uiset(v.begin(), v.end());
 
-	cout << "vec: ";
+	cout << "vec: \t";
 	for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
 		cout << *it << " ";
 	}
 	cout << endl;
 
-	cout << "set: ";
+	cout << "set: \t";
 	for (set<int>::iterator it = iset.begin(); it != iset.end(); it++) {
+		cout << *it << " ";
+	}
+	cout << endl;
+
+
+	cout << "set: \t";
+	for (set<int, myCmp>::iterator it = newiset.begin(); it != newiset.end(); it++) {
+		cout << *it << " ";
+	}
+	cout << endl;
+
+	cout << "unrodered_set: \t";
+	for (unordered_set<int>::iterator it = uiset.begin(); it != uiset.end(); it++) {
 		cout << *it << " ";
 	}
 	cout << endl;
--- string/find.cpp
+++ string/find.cpp
@@ -9,6 +9,15 @@ int main(int argc, char const *argv[])
 	auto it=s.find(sub);
 	cout << it << endl;
 	it=s.find("world");
+	cout << it << endl;
 	cout << boolalpha << (it==(string::npos)) << std::endl;
+
+	char flag[]="h";
+	int position=0, i=1;
+	while ((position=s.find(flag, position))!=-1) {
+		cout << "position " << i << ": " << position << endl;
+		++position;
+		++i;
+	}
 	return 0;
 }
